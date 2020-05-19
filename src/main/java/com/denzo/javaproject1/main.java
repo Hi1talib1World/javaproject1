8550c7bf948c9261a6fe123760ca5b3efe34d048
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-19 22:26:38 +0000

    Update main.java

--- src/main/java/com/denzo/javaproject1/main.java
+++ src/main/java/com/denzo/javaproject1/main.java
@@ -5,7 +5,7 @@ package com.denzo.javaproject1;
  * @author admin
  */
 public class main {
-    public void launch(){
+    public static void launch(String[] args){
         System.out.println("The sum of the given numbers:");
 
 }
