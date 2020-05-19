81e7ae67fafeb979a62cdfdd0c038182015ec758
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-19 09:01:12 +0000

    Initial commit

---
+++ src/main/java/com/denzo/javaproject1/user.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denzo.javaproject1;
+
+/**
+ *
+ * @author admin
+ */
+public class user {
+
+int x = 2020;
+int z = 1996;
+int y;
+public void addition(int x, int z) {
+      y = x + z;
+      System.out.println("The sum of the given numbers:"+y);
+   }
+
+}
