332b6d0614817b9a5fa00a6c96e2a79e75dbeb2f
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-20 01:03:40 +0000

    Create home.java

---
+++ src/main/java/com/denzo/javaproject1/home.java
@@ -0,0 +1,13 @@
+
+package com.denzo.javaproject1;
+/**
+ *
+ * @author admin
+ */
+public class home extends user{
+
+    public static void main(String[] args){
+            System.out.println("The sum of the given numbers:");
+
+    }
+}
