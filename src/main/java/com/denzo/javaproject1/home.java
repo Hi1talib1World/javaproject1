8ee6626df6ccf005f2a0e97ddc84fb1c99f4fed7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-20 16:14:58 +0000

    Update home.java

--- src/main/java/com/denzo/javaproject1/home.java
+++ src/main/java/com/denzo/javaproject1/home.java
@@ -4,10 +4,17 @@ package com.denzo.javaproject1;
  *
  * @author admin
  */
+import java.util.Date;  
+import java.text.SimpleDateFormat;  
+
+
 public class home{
 
     
     public static void main(String[] args){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+        Date date = new Date();  
+        System.out.println(formatter.format(date));
             user userObject = new user();
             userObject.addition();
 
